f9c3f3a004a0a1900bbde98822b4e697b94e137b
glowcoded <pavithrak7149@gmail.com>
2026-03-18 15:25:20 +0530

    Add code t0 uc3

---
+++ HelloApp/src/main/java/uc3_HelloApp.java
@@ -0,0 +1,9 @@
+public class uc3_HelloApp {
+    public static void main(String[] args) {
+
+        // Check if user provided a name
+
+        String name = args[0];
+        System.out.println("Hello " + name);
+    }
+}
