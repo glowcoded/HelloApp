1e3102b9de50feb5e09c1ab9d6b76b509995fe51
glowcoded <pavithrak7149@gmail.com>
2026-03-18 15:07:02 +0530

    Added uc2 code

---
+++ HelloApp/src/main/java/uc2_HelloApp.java
@@ -0,0 +1,9 @@
+public class uc2_HelloApp{
+public static void main(String[] args) {
+
+    // Check if user provided a name
+
+    String name = args[0];
+    System.out.println("Hello " + name);
+}
+}
